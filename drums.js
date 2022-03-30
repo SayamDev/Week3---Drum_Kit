db5afc2f49a7a83407d4ac2bb7c7480af77334dd
Sayam Ajmal <itssanesayam@gmail.com>
2022-03-30 16:56:37 +0100

    animation added via js

--- drums.js
+++ drums.js
@@ -6,12 +6,24 @@ const tom = document.getElementById("tom")
 //Music
 const soundTom = document.getElementById("musicTom");
 
-//keydown
+//keydown = sound and color being generated
 document.addEventListener("keydown", (event) => {
     if (event.key == "a" || event.key == "A") {
         soundTom.src = "sounds/tom.wav";
         tom.style.background = "#FCA311";
-        h2Tom.style.color = "#000000";
-        paraTom.style.color = "#000000";
+        p.style.fontSize = "12px";
+        h2.style.color = "#000000";
+        h2.style.fontSize = "30px";
+        p.style.color = "#000000";
+        
     }
+});
+
+//keyup = no color being generated
+
+document.addEventListener("keyup", () => {
+    h2.style.fontSize = "1.5em";
+    p.style.fontSize = "1.2rem";
+    tom.style.background = "#E5E5E5";
+
 })
