d4380a417e5518ea11871c5c50adc19ee5291c40
Sayam Ajmal <itssanesayam@gmail.com>
2022-03-31 09:44:02 +0100

    2nd key working

--- drums.js
+++ drums.js
@@ -1,10 +1,12 @@
 
 //button
-const tom = document.getElementById("tom")
+const tom = document.getElementById("tom");
+const tink = document.getElementById("tink");
 
 
 //Music
 const soundTom = document.getElementById("musicTom");
+const soundTink = document.getElementById("musicTink");
 
 //keydown = sound and color being generated
 document.addEventListener("keydown", (event) => {
@@ -12,11 +14,23 @@ document.addEventListener("keydown", (event) => {
         soundTom.src = "sounds/tom.wav";
         tom.style.background = "#FCA311";
         p.style.fontSize = "12px";
+        p.style.color = "#000000"; 
         h2.style.color = "#000000";
         h2.style.fontSize = "30px";
-        p.style.color = "#000000";
+       
+    } 
+});
+
+document.addEventListener("keydown", (event) => {
+    if (event.key == "s" || event.key == "S") {
+        soundTink.src = "sounds/tink.wav";
+        tink.style.background = "#FCA311";
+        p_2.style.fontSize = "12px";
+        p.style.color = "#000000";  
+        h2_2.style.color = "#000000";
+        h2_2.style.fontSize = "30px";
         
-    }
+    } 
 });
 
 //keyup = no color being generated
@@ -25,5 +39,9 @@ document.addEventListener("keyup", () => {
     h2.style.fontSize = "1.5em";
     p.style.fontSize = "1.2rem";
     tom.style.background = "#E5E5E5";
+    h2_2.style.fontSize = "1.5em";
+    p_2.style.fontSize = "1.2rem";
+    tink.style.background = "#E5E5E5";
+    
 
 })
