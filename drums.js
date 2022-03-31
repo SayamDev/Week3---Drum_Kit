557e56fd28f8600c22410e9d0b2c6f94715f3cc8
Sayam Ajmal <itssanesayam@gmail.com>
2022-03-31 09:58:53 +0100

    font colours black removed

--- drums.js
+++ drums.js
@@ -9,39 +9,62 @@ const soundTom = document.getElementById("musicTom");
 const soundTink = document.getElementById("musicTink");
 
 //keydown = sound and color being generated
+
+//keyA
 document.addEventListener("keydown", (event) => {
     if (event.key == "a" || event.key == "A") {
         soundTom.src = "sounds/tom.wav";
         tom.style.background = "#FCA311";
         p.style.fontSize = "12px";
-        p.style.color = "#000000"; 
-        h2.style.color = "#000000";
         h2.style.fontSize = "30px";
        
     } 
 });
 
+//keyS
 document.addEventListener("keydown", (event) => {
     if (event.key == "s" || event.key == "S") {
         soundTink.src = "sounds/tink.wav";
         tink.style.background = "#FCA311";
         p_2.style.fontSize = "12px";
-        p.style.color = "#000000";  
-        h2_2.style.color = "#000000";
         h2_2.style.fontSize = "30px";
         
     } 
 });
 
+
+//KeyD
+document.addEventListener("keydown", (event) => {
+    if (event.key == "d" || event.key == "D") {
+        soundTink.src = "sounds/snare.wav";
+        snare.style.background = "#FCA311";
+        p_3.style.fontSize = "12px";
+        h2_3.style.fontSize = "30px";
+        
+    } 
+});
+
+
+
+
+
+
+
 //keyup = no color being generated
 
 document.addEventListener("keyup", () => {
+    //KeyA
     h2.style.fontSize = "1.5em";
     p.style.fontSize = "1.2rem";
     tom.style.background = "#E5E5E5";
+    //KeyS
     h2_2.style.fontSize = "1.5em";
     p_2.style.fontSize = "1.2rem";
     tink.style.background = "#E5E5E5";
+    //KeyD
+    h2_3.style.fontSize = "1.5em";
+    p_3.style.fontSize = "1.2rem";
+    snare.style.background = "#E5E5E5";
     
 
 })
